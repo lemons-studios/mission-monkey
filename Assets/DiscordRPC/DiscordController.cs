cc5f3a72161dc6b3b6c5fa7d6631efd39bf86d82
Andrew Pinter <andrewpinter23@gmail.com>
2023-03-30 23:53:27 -0700

    Patch up the RPC a little

--- Assets/DiscordRPC/DiscordController.cs
+++ Assets/DiscordRPC/DiscordController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,13 @@ public class DiscordController : MonoBehaviour
 
     public string sDetails, sState, sLargeImage, sLargeText, sSmallImage, sSmallText;
 
+    private long startTime;
+
     void Start()
     {
         discord = new Discord.Discord(1090862646993096745, (System.UInt64)Discord.CreateFlags.Default);
         var activityManager = discord.GetActivityManager();
+        startTime = DateTimeOffset.Now.ToUnixTimeSeconds();
         var activity = new Discord.Activity
         {
             Details = sDetails,
@@ -24,6 +28,10 @@ public class DiscordController : MonoBehaviour
                 SmallImage = sSmallImage,
                 SmallText = sSmallText
             },
+            Timestamps =
+            {
+                Start = startTime
+            }
         };
         activityManager.UpdateActivity(activity, (res) => {
             if (res == Discord.Result.Ok)
@@ -35,6 +43,46 @@ public class DiscordController : MonoBehaviour
 
     void Update()
     {
+        var activityManager = discord.GetActivityManager();
+        var elapsedTime = DateTimeOffset.Now.ToUnixTimeSeconds() - startTime;
+        var activity = new Discord.Activity
+        {
+            Details = sDetails,
+            State = sState,
+            Assets = 
+            {
+                LargeImage = sLargeImage,
+                LargeText = sLargeText,
+                SmallImage = sSmallImage,
+                SmallText = sSmallText
+            },
+            Timestamps =
+            {
+                Start = startTime
+            },
+            Secrets =
+            {
+                Match = "matchId"
+            }
+        };
+        activityManager.UpdateActivity(activity, (res) => {
+            if (res == Discord.Result.Ok)
+                Debug.Log("Discord status set!");
+            else
+                Debug.LogError("Discord status failed!");
+        });
         discord.RunCallbacks();
+    }
+
+    void OnApplicationQuit()
+    {
+        var activityManager = discord.GetActivityManager();
+        activityManager.ClearActivity((res) => {
+            if (res == Discord.Result.Ok)
+                Debug.Log("Discord status cleared!");
+            else
+                Debug.LogError("Discord status clear failed!");
+        });
+        discord.Dispose();
     }
 }
