0500199ab19357d2ab583d0b6d247545363ee0bf
Andrew Pinter <andrewpinter23@gmail.com>
2023-03-31 20:53:25 -0700

    Add https://github.com/dyrdadev/first-person-controller-for-unity#features & deps

--- Assets/UniversalScripts/PlayerController/movementControls.cs
+++ Assets/UniversalScripts/PlayerController/movementControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class movementControls : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     void Start()
     {
         
@@ -13,6 +14,6 @@ public class movementControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
