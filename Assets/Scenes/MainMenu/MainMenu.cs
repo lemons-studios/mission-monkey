0ab88c0bd99ddd8b0b5320bdf515eed8663fa09e
ChakornK <chakornk2007@gmail.com>
2023-04-03 22:40:53 -0700

    added menu elements

---
+++ Assets/Scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void Play () {
+        SceneManager.LoadScene("Prototyping Scene");
+    }
+}
