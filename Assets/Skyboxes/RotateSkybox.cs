c84937fe8785907e6888f542b303eae5a4d2881f
ChakornK <chakornk2007@gmail.com>
2023-04-03 19:30:59 -0700

    added main menu with rotating skybox

---
+++ Assets/Skyboxes/RotateSkybox.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+ 
+public class RotateSkybox : MonoBehaviour
+{
+ 
+    [SerializeField] Volume volume;
+    HDRISky sky;
+    public float RotateSpeed = 0.02f;
+ 
+    private void Start()
+    {
+        volume.profile.TryGet(out sky);
+    }
+ 
+    private void FixedUpdate()
+    {
+        /// ROTATE HDRI SKYBOX
+        sky.rotation.value += RotateSpeed;
+    }
+}
