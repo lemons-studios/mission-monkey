947665194557e630d5bf3fe3da5368369c71df85
ChakornK <chakornk2007@gmail.com>
2023-04-06 23:01:47 -0700

    fixed main menu buttons

--- Assets/Scenes/MainMenu/MainMenu.cs
+++ Assets/Scenes/MainMenu/MainMenu.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    public void Play(){
-        SceneManager.LoadScene(4);
-    }
     public void Load(){
-        
+
+    }
+    public void Play(){
+        SceneManager.LoadScene("Prototyping Scene");
     }
     public void Quit() {
         Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
 
