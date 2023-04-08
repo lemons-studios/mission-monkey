150bb7544484c096dd076821b642c0d1c0e3c94b
ChakornK <chakornk2007@gmail.com>
2023-04-07 22:46:40 -0700

    camera sensitivity works now

--- Assets/UniversalScripts/PlayerController/Scripts/PlayerLook.cs
+++ Assets/UniversalScripts/PlayerController/Scripts/PlayerLook.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+
+    public static float[] sens = {xSens, ySens};
+    public static float xSens = 30f;
+    public static float ySens = 30f;
     public Camera cam;
     public float xRotation = 0f;
 
-    public static float xSens = 30f;
-    public static float ySens = 30f;
-
-    public static float[] sens = {xSens, ySens};
     // Start is called before the first frame update
     public void ProcessLook(Vector2 input) {
         float mouseX = input.x;
