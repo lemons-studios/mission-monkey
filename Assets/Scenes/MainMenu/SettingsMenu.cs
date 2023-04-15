db325923cd93db8fa368c1ffb94d0af2fa847f11
ChakornK <chakornk2007@gmail.com>
2023-04-14 17:55:58 -0700

    added quality dropdown selector

--- Assets/Scenes/MainMenu/SettingsMenu.cs
+++ Assets/Scenes/MainMenu/SettingsMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -10,9 +11,11 @@ public class SettingsMenu : MonoBehaviour
     public Slider fovSlider;
     public Slider msSlider;
     public GameObject optionMenu;
+    public TMP_Dropdown qualitySelect;
     public Slider volSlider;
     float fov;
     float mouseSens;
+    int quality;
     float volume;
     public void FOV(float fov) {
         // GameObject.Find("OptionsMenu").GetComponent<CameraFOV>().setCameraFOV(fov);
@@ -30,6 +33,13 @@ public class SettingsMenu : MonoBehaviour
             msSlider.value = sens;
         }
     }
+    public void Quality(int index) {
+        QualitySettings.SetQualityLevel(index, true);
+        PlayerPrefs.SetInt("Quality", index);
+        if (qualitySelect.value != index) {
+            qualitySelect.value = index;
+        }
+    }
     public void Volume(float volume) {
         audiomixer.SetFloat("Volume", volume * volume * volume / 6400);
         // audiomixer.SetFloat("Volume", volume * volume * volume * volume / -512000);
@@ -42,10 +52,12 @@ public class SettingsMenu : MonoBehaviour
     {
         fov = PlayerPrefs.GetFloat("CameraFOV", 60);
         mouseSens = PlayerPrefs.GetFloat("MouseSens", 80);
+        quality = PlayerPrefs.GetInt("Quality", 4);
         volume = PlayerPrefs.GetFloat("Volume", -5);
 
         FOV(fov);
         MouseSens(mouseSens);
+        Quality(quality);
         Volume(volume);
     }
 }
