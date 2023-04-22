3f6997299ad3301f6048c293df857983cab6175e
ChakornK <chakornk2007@gmail.com>
2023-04-22 14:30:39 -0700

    fixed tooltip text

--- Assets/Universal/Scripts/PlayerController/Scripts/PlayerUI.cs
+++ Assets/Universal/Scripts/PlayerController/Scripts/PlayerUI.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 public class PlayerUI : MonoBehaviour
 {
+    public GameObject promptTextContainer;
     // Start is called before the first frame update
     [SerializeField]
     private TextMeshProUGUI promptText;
-    public GameObject promptTextContainer;
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     public void UpdateText(string promptMessage)
@@ -22,5 +18,9 @@ public class PlayerUI : MonoBehaviour
             promptTextContainer.SetActive(true);
         }
         promptText.text = promptMessage;
+    }
+    void Start()
+    {
+
     }
 }
