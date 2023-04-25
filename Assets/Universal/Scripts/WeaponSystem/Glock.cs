1fcd2d65771db20f2d706d2bd48287173fb2323b
ChakornK <chakornk2007@gmail.com>
2023-04-24 18:52:50 -0700

    glock script finished but doesnt work yet

--- Assets/Universal/Scripts/WeaponSystem/Glock.cs
+++ Assets/Universal/Scripts/WeaponSystem/Glock.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Glock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera cam;
+    public Transform firePoint;
+    public PlayerInput.OnFootActions onFoot;
+    public GameObject projectile;
+
+    private Vector3 destination;
+    private PlayerInput playerInput;
+
+    void Awake()
     {
-        
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        onFoot.GunFire.performed += ctx => UnityEngine.Debug.Log("bullet");;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void InstantiateProjectile(Transform point) {
+        var projectileObj = Instantiate(projectile, point.position, Quaternion.identity) as GameObject;
+    }
+
+    void ShootProjectile() {
+        UnityEngine.Debug.Log("shot a bullet");
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            destination = hit.point;
+        } else {
+            destination = ray.GetPoint(1000);
+        }
+
+        InstantiateProjectile(firePoint);
+    }
+
+    void Start() {
+
     }
 }
