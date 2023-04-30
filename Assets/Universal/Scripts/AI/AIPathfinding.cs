8a8575fcc58c15840ff407aa423038a060cc13a8
Shob3r <andrewpinter23@gmail.com>
2023-04-30 16:38:38 -0700

    add AI pathfinding

---
+++ Assets/Universal/Scripts/AI/AIPathfinding.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIPathfinding : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            IterateWaypointIndex();
+            UpdateDestination();
+        }
+    }
+    void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+    }
+    void IterateWaypointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
