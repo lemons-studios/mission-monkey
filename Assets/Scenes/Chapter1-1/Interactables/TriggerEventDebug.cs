ea834fc25d8ac2b75c5712b7736ea79589cb3691
Shob3r <andrewpinter23@gmail.com>
2023-05-09 10:30:33 -0700

    EXPLOSION

--- Assets/Scenes/Chapter1-1/Interactables/TriggerEventDebug.cs
+++ Assets/Scenes/Chapter1-1/Interactables/TriggerEventDebug.cs
@@ -1,14 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerEventDebug : Interactable
 {
-    private bool EventTriggered;
-    public GameObject PrisonBars;
+    private bool EventTriggered = false;
+
+    public GameObject[] PrisonBars;
+    public Rigidbody[] BarsRigidBody;
+    public float ExplosionForce = 20f;
+    private float DestroyTransitionTime = 2.5f;
+
+    private void Awake()
+    {
+        // PrisonBars = new GameObject[5];
+        BarsRigidBody = new Rigidbody[5];
+        for (int i = 0; i < PrisonBars.Length; i++)
+        {
+            BarsRigidBody[i] = PrisonBars[i].GetComponent<Rigidbody>();
+        }
+        Debug.Log(BarsRigidBody);
+    }
+
     protected override void Interact()
     {
-        EventTriggered = !EventTriggered;
-        PrisonBars.GetComponent<Animator>().SetBool("BarsExplodeEventTriggered", EventTriggered);
+        if (EventTriggered) return;
+        EventTriggered = true;
+        for (int i = 0; i < BarsRigidBody.Length; i++)
+        {
+            BarsRigidBody[i].useGravity = true;
+            BarsRigidBody[i].AddForce(Vector3.back * ExplosionForce);
+            Destroy(PrisonBars[i], DestroyTransitionTime);
+        }
+        Debug.Log("Event triggered!");
     }
+    
 }
