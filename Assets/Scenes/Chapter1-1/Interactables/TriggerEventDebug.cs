2b11a631bbe0b55e6ece77db4d7f9ea0fa4a668c
Shob3r <andrewpinter23@gmail.com>
2023-05-11 09:50:22 -0700

    ADDHHADHSH

--- Assets/Scenes/Chapter1-1/Interactables/TriggerEventDebug.cs
+++ Assets/Scenes/Chapter1-1/Interactables/TriggerEventDebug.cs
@@ -6,8 +6,9 @@ public class TriggerEventDebug : Interactable
 
     public GameObject[] PrisonBars;
     public Rigidbody[] BarsRigidBody;
-    public float ExplosionForce = 20f;
+    public Material TransitionMaterial;
     private float DestroyTransitionTime = 2.5f;
+    private bool DisplayDistroyTransition = false;
 
     private void Awake()
     {
@@ -28,9 +29,25 @@ public class TriggerEventDebug : Interactable
         {
             BarsRigidBody[i].useGravity = true;
             BarsRigidBody[i].AddForce(Vector3.back * Random.Range(30.0f, 99.99f));
-            Destroy(PrisonBars[i], DestroyTransitionTime);
         }
+        DisplayDistroyTransition = true;
         Debug.Log("Event triggered!");
     }
-    
+
+    private void Update()
+    {
+        if (DisplayDistroyTransition)
+        {
+            if (TransitionMaterial.color.a >= 0.69420)
+            {
+                Color mat = TransitionMaterial.color;
+                mat.a -= 0.2f;
+                TransitionMaterial.color = mat;
+            } else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
 }
