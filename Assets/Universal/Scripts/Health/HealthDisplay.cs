c4960af5df489368f961fd45fabf2b8bd25dc6ce
ChakornK <chakornk2007@gmail.com>
2023-05-24 13:30:23 -0700

    add health bar, todo: fix color not changing

--- Assets/Universal/Scripts/Health/HealthDisplay.cs
+++ Assets/Universal/Scripts/Health/HealthDisplay.cs
@@ -1,28 +1,32 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
+    public GameObject HealthBar;
     public TextMeshProUGUI HealthText;
     private float TransitionTime = 0.5f;
-    private Color fullHealthColour = new Color(0, 255, 0);
-    private Color HalfHealthColor = new Color(255, 166, 0);
-    private Color LowHealthColor = new Color(231, 0, 0);
+    private Color fullHealthColour = new Color(94, 185, 93);
+    private Color HalfHealthColor = new Color(195, 173, 47);
+    private Color LowHealthColor = new Color(241, 101, 61);
 
     void Update()
     {
         HealthText.text = PlayerHealth.Health.ToString();
         if (PlayerHealth.Health >= 65f)
         {
-            HealthText.color = fullHealthColour;
+            HealthBar.GetComponent<Image>().color = fullHealthColour;
         }
         if (PlayerHealth.Health >= 31f && PlayerHealth.Health <= 64f)
         {
-            HealthText.color = HalfHealthColor;
+            HealthBar.GetComponent<Image>().color = HalfHealthColor;
         }
         if (PlayerHealth.Health >= 1f && PlayerHealth.Health <= 30f)
         {
-            HealthText.color = LowHealthColor;
+            HealthBar.GetComponent<Image>().color = LowHealthColor;
         }
+
+        HealthBar.GetComponent<RectTransform>().offsetMax = new Vector2(-369f + PlayerHealth.Health * 3.69f, 0f);
     }
 }
