74845a255657964bb4b27fc0154fa641f9b43738
Shob3r <andrewpinter23@gmail.com>
2023-06-13 18:38:35 -0700

    the prison room models are basically done Maybe some different materials will be needed, but gameplay is what is needed now

--- Assets/Scenes/Chapter1-1/Interactables/BarrelExplosion.cs
+++ Assets/Scenes/Chapter1-1/Interactables/BarrelExplosion.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class BarrelExplosion : MonoBehaviour
 {
     public GameObject Barrel;
+    public BoxCollider[] PrisonBarColliders;
+    public BoxCollider[] ShootingRangeColliders;
     public Rigidbody[] BarsRigidBody;
+    public Rigidbody[] ShootingRangeRigidBody;
     public GameObject ExplosionParticles;
     public GameObject[] PrisonBars;
+    public GameObject[] ShootingRangeBars;
     public Material TransitionMaterial;
     public AudioSource barsEffectSource;
     public float maxRange = 100f;
@@ -23,19 +27,38 @@ public class BarrelExplosion : MonoBehaviour
         {
             BarsRigidBody[i].useGravity = true;
             BarsRigidBody[i].AddForce(Vector3.forward * Random.Range(minRange, maxRange));
+            //PrisonBarColliders[i].size = new Vector3(0.1f,0.1f,0.1f);
+
+        }
+        for (int i = 0; i < ShootingRangeRigidBody.Length; i++)
+        {
+            ShootingRangeRigidBody[i].useGravity = true;
+            ShootingRangeRigidBody[i].AddForce(Vector3.right * Random.Range(100, 150));
+            //ShootingRangeColliders[i].size = new Vector3(0.1f,0.1f,0.1f);
+
         }
         barsEffectSource.Play();
-        // StartCoroutine(WaitBeforeDestroy());
-        // fadeOut();
         Destroy(gameObject);
+
+
     }
 
     private void Awake()
     {
         BarsRigidBody = new Rigidbody[5];
+        PrisonBarColliders = new BoxCollider[5];
+        ShootingRangeColliders = new BoxCollider[5];
+
         for (int i = 0; i < PrisonBars.Length; i++)
         {
             BarsRigidBody[i] = PrisonBars[i].GetComponent<Rigidbody>();
+            PrisonBarColliders[i] = PrisonBars[i].GetComponent<BoxCollider>();
+        }
+        ShootingRangeRigidBody = new Rigidbody[5];
+        for (int i = 0; i < ShootingRangeBars.Length; i++)
+        {
+            ShootingRangeRigidBody[i] = ShootingRangeBars[i].GetComponent<Rigidbody>();
+            ShootingRangeColliders[i] = ShootingRangeBars[i].GetComponent<BoxCollider>();
         }
     }
 }
