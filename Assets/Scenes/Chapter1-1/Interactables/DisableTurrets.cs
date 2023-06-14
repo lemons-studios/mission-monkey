0e567ee9809eccf4e3bab3bddb240ea1019ada4b
Shob3r <andrewpinter23@gmail.com>
2023-06-14 13:59:09 -0700

    heeeheeeheeheee

---
+++ Assets/Scenes/Chapter1-1/Interactables/DisableTurrets.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTurrets : Interactable
+{
+    protected override void Interact()
+    {
+        base.Interact();
+    }
+}
