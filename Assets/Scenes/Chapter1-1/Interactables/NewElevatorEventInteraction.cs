38e6c7f2bdf9993b1c23a6cee45bc9505a6ff091
Shob3r <andrewpinter23@gmail.com>
2023-06-16 14:53:52 -0700

    add a slightly broken new elevator interaction event

---
+++ Assets/Scenes/Chapter1-1/Interactables/NewElevatorEventInteraction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewElevatorEventInteraction : Interactable
+{
+    public GameObject NewElevator;
+    public GameObject Player;
+    public bool isEventTriggered;
+    
+    protected override void Interact()
+    {
+        NewElevator.GetComponent<Animator>().SetBool("IsInteractedWith", isEventTriggered);
+        Player.GetComponent<PlayerMotor>().speed = 0f;
+    }
+}
