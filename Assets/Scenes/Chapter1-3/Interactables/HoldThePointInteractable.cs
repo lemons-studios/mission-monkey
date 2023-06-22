7e99ef1bd746a99ab6eded5b46ad238909788c4c
Shob3r <andrewpinter23@gmail.com>
2023-06-21 23:37:31 -0700

    the waves do not work and i do not have the time to fix them 0.2.0 will ship with this bug, there will be a hotfix when i have time that fixes this though almost done

--- Assets/Scenes/Chapter1-3/Interactables/HoldThePointInteractable.cs
+++ Assets/Scenes/Chapter1-3/Interactables/HoldThePointInteractable.cs
@@ -6,7 +6,6 @@ using UnityEngine.Audio;
 public class HoldThePointInteractable : Interactable
 {
     public GameObject[] Monkeys;
-    public GameObject[] Wave1, Wave2, Wave3, Wave4, Wave5, Wave6;
     public GameObject LeaveInteractable;
     public Light LeaveEscapePodLight;
     public TextMeshProUGUI CountDown;
@@ -34,58 +33,40 @@ public class HoldThePointInteractable : Interactable
                 LeaveInteractable.gameObject.SetActive(true);
                 LeaveEscapePodLight.gameObject.SetActive(true);
             }
-            if (Roundedtime == 130 & monkeyEnabler == 0|| Roundedtime == 105 & monkeyEnabler == 1 || Roundedtime == 80 & monkeyEnabler == 2 || Roundedtime == 55 & monkeyEnabler == 3 || Roundedtime == 30 & monkeyEnabler == 4|| Roundedtime == 20 & monkeyEnabler == 5 )
+            if (Roundedtime == 130 & monkeyEnabler == 0 || Roundedtime == 105 & monkeyEnabler == 1 || Roundedtime == 80 & monkeyEnabler == 2 || Roundedtime == 55 & monkeyEnabler == 3 || Roundedtime == 30 & monkeyEnabler == 4 || Roundedtime == 20 & monkeyEnabler == 5)
             {
                 SpawnWave();
+            }
+            if (Roundedtime == 130)
+            {
+                Chapter1_3Wave1.isWaveReady = true;
+            }
+            if (Roundedtime == 105)
+            {
+                Chapter1_3Wave2.isWaveReady = true;
+            }
+            if (Roundedtime == 80)
+            {
+                Chapter1_3Wave3.isWaveReady = true;
+            }
+            if (Roundedtime == 55)
+            {
+                Chapter1_3Wave4.isWaveReady = true;
+            }
+            if (Roundedtime == 30)
+            {
+                Chapter1_3Wave5.isWaveReady = true;
+            }
+            if (Roundedtime == 20)
+            {
+                Chapter1_3Wave6.isWaveReady = true;
             }
         }
     }
     void SpawnWave()
     {
-        
-        if (monkeyEnabler == 0)
-        {
-            for (int i = 0; i < Wave1.Length; i++)
-            {
-                Wave1[i].SetActive(true);
-            }
-        }
-        else if (monkeyEnabler == 1)
-        {
-            for (int i = 0; i < Wave2.Length; i++)
-            {
-                Wave2[i].SetActive(true);
-            }
-        }
-        else if (monkeyEnabler == 2)
-        {
-            for (int i = 0; i < Wave3.Length; i++)
-            {
-                Wave3[i].SetActive(true);
-            }
-        }
-        else if (monkeyEnabler == 3)
-        {
-            for (int i = 0; i < Wave4.Length; i++)
-            {
-                Wave4[i].SetActive(true);
-            }
-        }
-        else if (monkeyEnabler == 4)
-        {
-            for (int i = 0; i < Wave5.Length; i++)
-            {
-                Wave5[i].SetActive(true);
-            }
-        }
-        else if (monkeyEnabler == 5)
-        {
-            for (int i = 0; i < Wave6.Length; i++)
-            {
-                Wave6[i].SetActive(true);
-            }
-        }
         Monkeys[monkeyEnabler].SetActive(true);
         monkeyEnabler++;
     }
+
 }
