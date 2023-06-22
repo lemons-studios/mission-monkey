ef68df7b374079f6fd2dea5d8b6189a986055a3e
Shob3r <andrewpinter23@gmail.com>
2023-06-22 00:11:01 -0700

    last change that prevents some errors

--- Assets/Scenes/Chapter1-3/Interactables/HoldThePointInteractable.cs
+++ Assets/Scenes/Chapter1-3/Interactables/HoldThePointInteractable.cs
@@ -13,6 +13,7 @@ public class HoldThePointInteractable : Interactable
     public float Roundedtime = 150f;
     int monkeyEnabler = 0;
     bool ButtonPushed = false;
+    int escapeSequenceOver = 0;
 
     protected override void Interact()
     {
@@ -27,11 +28,12 @@ public class HoldThePointInteractable : Interactable
             TimeSpan timeSpan = TimeSpan.FromSeconds(TimeLeft);
             string formattedTimeLeft = timeSpan.ToString(@"mm\:ss");
             CountDown.text = "Time Until All Monkeys Board: " + formattedTimeLeft;
-            if (TimeLeft <= 0)
+            if (TimeLeft <= 0 & escapeSequenceOver == 0)
             {
                 CountDown.text = "All the Monkeys Have Boarded. Escape Before This Space Station Blows Up!";
                 LeaveInteractable.gameObject.SetActive(true);
                 LeaveEscapePodLight.gameObject.SetActive(true);
+                escapeSequenceOver++;
             }
             if (Roundedtime == 130 & monkeyEnabler == 0 || Roundedtime == 105 & monkeyEnabler == 1 || Roundedtime == 80 & monkeyEnabler == 2 || Roundedtime == 55 & monkeyEnabler == 3 || Roundedtime == 30 & monkeyEnabler == 4 || Roundedtime == 20 & monkeyEnabler == 5)
             {
