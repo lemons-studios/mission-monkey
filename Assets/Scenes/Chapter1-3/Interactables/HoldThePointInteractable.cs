ded3dc62d1434252e31e014f42e13d4413edb1a0
Shob3r <andrewpinter23@gmail.com>
2023-06-21 21:09:23 -0700

    Implement Timer Script, gameplay for 1-3 going well!

--- Assets/Scenes/Chapter1-3/Interactables/HoldThePointInteractable.cs
+++ Assets/Scenes/Chapter1-3/Interactables/HoldThePointInteractable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
 
@@ -7,9 +8,23 @@ public class HoldThePointInteractable : Interactable
 {
     public GameObject[] Monkeys;
     public TextMeshProUGUI CountDown;
+    public float TimeLeft = 150f;
+    bool ButtonPushed = false;
 
     protected override void Interact()
     {
-        
+        ButtonPushed = true;
+    }
+    void Update()
+    {
+        if(ButtonPushed == true)
+        {
+            TimeLeft -= Time.deltaTime;
+            CountDown.text = "Time Until Self-Destruct: " + TimeLeft.ToString("0");
+            if(TimeLeft <= 0)
+            {
+                
+            }
+        }
     }
 }
