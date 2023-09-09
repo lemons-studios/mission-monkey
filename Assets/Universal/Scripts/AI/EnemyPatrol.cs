41eb5d9edfc7272834ec7d8a2f7061070752c5a9
Karl <karlonasa12@gmail.com>
2023-09-08 21:15:58 -0700

    added waypoints and made enemy patrol

--- Assets/Universal/Scripts/AI/EnemyPatrol.cs
+++ Assets/Universal/Scripts/AI/EnemyPatrol.cs
@@ -6,9 +6,41 @@ using UnityEngine.AI;
 public class EnemyPatrol : State
 {
     public NavMeshAgent agent;
+    public Transform[] wayPoints;
+    public int currentPoint = 0;
+    public bool switchRoute;
     public override State RunCurrentState()
     {
-       
+
+        if(transform.position.x != wayPoints[currentPoint].position.x && transform.position.z != wayPoints[currentPoint].position.z)
+        {
+            agent.SetDestination(wayPoints[currentPoint].position);
+        }
+        else
+        {
+            if(currentPoint !> wayPoints.Length && switchRoute == true)
+            {
+
+                currentPoint = currentPoint + 1;
+
+            }
+            else if(currentPoint > wayPoints.Length && switchRoute == true)
+            {
+                switchRoute = false;
+            }
+
+            if(currentPoint > wayPoints.Length && switchRoute == false)
+            {
+                currentPoint = currentPoint - 1;
+            }
+            else if(currentPoint < 0 && switchRoute == false)
+            {
+                switchRoute = true;
+            }
+            
+               
+        }
+        
 
         return this;
     }
