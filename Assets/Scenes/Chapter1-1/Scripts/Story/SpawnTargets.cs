f6c70fb67972865d8e493286b65ab01948f0c8ba
Shob3r <andrewpinter23@gmail.com>
2023-11-05 18:35:14 -0800

    The 0.3.1 grind is back

--- Assets/Scenes/Chapter1-1/Scripts/Story/SpawnTargets.cs
+++ Assets/Scenes/Chapter1-1/Scripts/Story/SpawnTargets.cs
@@ -5,7 +5,7 @@ public class SpawnTargets : MonoBehaviour
     public GameObject[] Targets;
     public void EnableTargets()
     {
-        Debug.Log("Weapon Picked Up!");
+        // Debug.Log("Weapon Picked Up!");
         foreach(GameObject Target in Targets)
         {
             Target.SetActive(true);
