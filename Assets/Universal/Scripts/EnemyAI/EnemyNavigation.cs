c328ef349185416240bad35613ac80c44e3975b8
Shob3r <andrewpinter23@gmail.com>
2024-02-23 17:25:09 -0800

    NAVIGATION WORKS!! finally, it took too long for this chapter 1 works after I finish the AI attack script

--- Assets/Universal/Scripts/EnemyAI/EnemyNavigation.cs
+++ Assets/Universal/Scripts/EnemyAI/EnemyNavigation.cs
@@ -5,28 +5,81 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyNavigation : MonoBehaviour
 {
-    public GameObject[] patrolPoints;
+    public Transform[] patrolPoints;
+    private EnemySight enemySight;
     private NavMeshAgent agent;
-
-    public float rotationFrequency = 0.25f;
+    
+    private int currentTarget = 0;
     public float navigationUpdateFrequency = 0.15f;
+    private bool seenPlayer = false;
 
     private void Start() 
     {
+        enemySight = GetComponent<EnemySight>();
         agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Navigate());
     }
 
-    private IEnumerator FaceTarget(GameObject target)
+    private void Update()
     {
-        // Shamelessly stealing Quaternion code because I despise math (It pains me to know that I will have to learn 3d math one day)
+        Vector3 currentTargetPosition = GetCurrentTargetPosition();
+        Quaternion targetRotation = Quaternion.LookRotation(currentTargetPosition - transform.position);
+        float rotationSpeed =  0.2f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator Navigate()
+    {
         while(true)
         {
-            Vector3 targetPosition = target.transform.position; 
-            targetPosition.y = 0;   // Prevents the ai from getting silly on the Y axis (I think?)
-            Quaternion targetRotation = Quaternion.LookRotation(targetPosition);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.2f);    
+            if(seenPlayer)
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                agent.destination = player.transform.position;
+            }
 
-            yield return new WaitForSeconds(rotationFrequency);
+            else
+            {
+                // Patrol point navigation if there are any
+                if(patrolPoints != null)
+                {
+                    if(agent.remainingDistance <= agent.stoppingDistance)
+                    {
+                        Debug.Log(gameObject.name + " reached its destination. going to next destination");
+                        currentTarget = GetNextPatrolPointIndex(patrolPoints, currentTarget);
+                    }    
+                    agent.destination = patrolPoints[currentTarget].position;
+                }
+
+                // Check if the AI has noticed the player 
+                if(enemySight.isPlayerVisible())
+                {
+                    Debug.Log(gameObject.name + " has found the player!");
+                    seenPlayer = true;
+                }
+            }
+            yield return new WaitForSeconds(navigationUpdateFrequency);
+        }
+    }
+    
+    private int GetNextPatrolPointIndex(Transform[] patrolPointArray, int currentElement)
+    {
+        if(currentTarget + 1 > patrolPointArray.Length - 1)
+        {
+            return 0;   // Start back at beginning of array
+        }
+        else return currentElement + 1;
+    }
+
+    private Vector3 GetCurrentTargetPosition()
+    {
+        if(seenPlayer)
+        {
+            return GameObject.FindGameObjectWithTag("Player").transform.position;
+        }
+        else
+        {
+            return patrolPoints[currentTarget].position;
         }
     }
 }
